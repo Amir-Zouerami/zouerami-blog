cd9a9952539737c0394abee0d5901c6c85263848
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2023-08-18 11:20:03 +0330

    - Added active-link styles as placeholder for now
    - Adjusted some padding and sorted orders of the left-side icons on Navbar.

--- components/Nav/Navbar.tsx
+++ components/Nav/Navbar.tsx
@@ -10,7 +10,7 @@ import Link from 'next/link';
 function Navbar() {
   return (
     // <nav className="fixed right-1/2 top-0 h-16 w-full translate-x-1/2 bg-gray-200 md:w-2/3">
-    <nav className="sticky top-0 py-1">
+    <nav className="sticky top-0 pt-3">
       <div className="">
         <div className="flex items-center justify-between">
           <div className="p-4 md:hidden">
@@ -19,9 +19,12 @@ function Navbar() {
             </Link>
           </div>
 
-          <ul className="hidden pr-8 md:flex md:gap-20">
+          <ul className="hidden md:flex md:gap-20">
             <li>
-              <Link href={'#'} className="">
+              <Link
+                href={'#'}
+                className="mr-0 rounded-[10px] bg-[#1f2124] px-4 py-2 font-semibold text-white"
+              >
                 صفحه اصلی
               </Link>
             </li>
@@ -51,9 +54,9 @@ function Navbar() {
             </li>
           </ul>
 
-          <div className="flex flex-row-reverse space-x-5 p-4">
+          <div className="flex gap-2 p-5 md:gap-4">
             <Link href={'#'}>
-              <Image src={moon} alt="dark-mode" width={25} />
+              <Image src={search} alt="search" width={25} />
             </Link>
 
             <Link href={'#'}>
@@ -61,7 +64,7 @@ function Navbar() {
             </Link>
 
             <Link href={'#'}>
-              <Image src={search} alt="search" width={25} />
+              <Image src={moon} alt="dark-mode" width={25} />
             </Link>
           </div>
         </div>
