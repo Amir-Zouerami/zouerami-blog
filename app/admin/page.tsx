bd95e57f9779fe44dbb4b1912a8d6e70f143f232
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2023-09-22 09:26:36 +0330

    ROUTE: Added `Admin` PlaceHolder Route.

---
+++ app/admin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function page() {
+  return (
+    <div className="mx-auto mt-20 max-w-[95%] lg:max-w-[1200px]">
+      <p>TESTING</p>
+    </div>
+  );
+}
+
+export default page;
