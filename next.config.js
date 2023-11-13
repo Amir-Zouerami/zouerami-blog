9e4adb47fc4216313f270a5bf540ee31a9f1b50b
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2023-11-13 12:13:20 +0330

    CONFIG: Change Next.js Config `remotePatterns` To Accept From Pocketbase (Localhost).

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    remotePatterns: [{ protocol: 'http', hostname: '127.0.0.1', port: '8090' }],
+  },
   // experimental: {
   //   serverActions: true,
   //   serverActionsBodySizeLimit: '3mb',
