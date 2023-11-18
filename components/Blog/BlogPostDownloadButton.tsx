8d0c02b295798fb973fea2e4fcd12f4cdd20f2c1
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2023-11-18 14:28:19 +0330

    LOGIC: Created `BlogPostDownloadButton` To Contain Custom Logic For Showing The Download Article Button (NO PLANS YET!).

---
+++ components/Blog/BlogPostDownloadButton.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import download from '@/icons/download.svg';
+
+function BlogPostDownloadButton() {
+  return (
+    <div className="my-10 text-center">
+      <Link
+        href={'#'}
+        onClick={() => {
+          toast(
+            `این قابلیت در حال حاضر در دسترس نیست.
+              در آینده ی نزدیک کاربران ویژه می توانند مقالات سایت را دانلود کنند.`,
+            { icon: '📢', duration: 4000 }
+          );
+        }}
+        className="w-10 rounded-xl bg-[#FE634E] px-12 py-4"
+      >
+        <Image
+          width={25}
+          src={download}
+          alt="download article"
+          className="inline-block align-text-bottom"
+        />
+        <span className="pr-2 font-bold text-white"> PDF </span>
+      </Link>
+
+      <Toaster />
+    </div>
+  );
+}
+
+export default BlogPostDownloadButton;
