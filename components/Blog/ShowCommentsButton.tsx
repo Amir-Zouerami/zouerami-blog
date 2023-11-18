f3544fc77dea1820d74f0cb82604bac017b7d13a
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2023-11-18 14:32:41 +0330

    LOGIC: Created A Custom Component `ShowCommentsButton` To Contain Custom Logic For Displaying/Hiding Comments Modal.

---
+++ components/Blog/ShowCommentsButton.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import Link from 'next/link';
+import { atom, useAtom } from 'jotai';
+import comment from '@/icons/comment.svg';
+import Image from 'next/image';
+import { useEffect } from 'react';
+
+export const commentsModalAtom = atom(false);
+
+function BlogCommentsButton() {
+  const [commentsModal, setCommentsModal] = useAtom(commentsModalAtom);
+
+  useEffect(() => {
+    window.addEventListener('keydown', key => {
+      if (key.code === 'Escape') {
+        setCommentsModal(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener('keydown', key => {
+        if (key.code === 'Escape') {
+          setCommentsModal(false);
+        }
+      });
+    };
+  }, [setCommentsModal]);
+
+  return (
+    <div className="mt-16 text-center">
+      <Link
+        href={'#'}
+        scroll={false}
+        className="w-10 rounded-xl bg-gradient-to-r from-[#5e4b55] to-[#da8b83] px-12 py-4 dark:from-[#5AA68C] dark:to-[#C0B35F]"
+        onClick={() => {
+          setCommentsModal(!commentsModal);
+        }}
+      >
+        <Image
+          className="inline"
+          src={comment}
+          width={20}
+          alt="Open Comments Modal"
+        />
+        <span className="pr-2 font-bold text-white">مشاهده نظرات</span>
+      </Link>
+    </div>
+  );
+}
+
+export default BlogCommentsButton;
