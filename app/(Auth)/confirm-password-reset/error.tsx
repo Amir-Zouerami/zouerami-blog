002ad8680d0d5f0bc0640bea0c2f2cb32b9a4c1f
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2023-11-28 21:44:42 +0330

    LOGIC: use a dedicated error page for password-reset functionality.

---
+++ app/(Auth)/confirm-password-reset/error.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import Error500 from '@/icons/Error500';
+import Link from 'next/link';
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="mx-auto mt-24 flex max-w-[800px] flex-col items-center justify-center">
+      <h2 className="mb-10 text-4xl font-black">
+        <span className="text-red-400">خطایی</span> رخ داده است ... !
+      </h2>
+
+      <div className="w-full text-center">
+        <Error500 margin="lg:-mb-8" />
+        <p className="text-xs text-gray-400">
+          Illustration & Animation By{' '}
+          <Link
+            className="-mt-10 underline hover:cursor-pointer hover:text-cyan-500"
+            href={'https://codepen.io/henrywr'}
+          >
+            Henry W
+          </Link>
+        </p>
+      </div>
+
+      <p className="mb-10 mt-5 max-w-[500px] px-4 text-justify text-lg font-bold leading-9">
+        لینک تغییر رمز حساب کاربری شما{' '}
+        <span className="text-orange-500"> صحیح نیست </span>و یا
+        <span className="text-orange-500"> منقضی شده </span>
+        است.
+      </p>
+    </div>
+  );
+}
