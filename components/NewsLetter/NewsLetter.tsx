cb05b5deda12ec01d03a208f2f7a1196dd1fb8d5
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2023-11-28 22:47:48 +0330

    LOGIC: `NewsLetter` on main page (`/`) now redirects users to profile settings.

--- components/NewsLetter/NewsLetter.tsx
+++ components/NewsLetter/NewsLetter.tsx
@@ -1,20 +1,36 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import Pocketbase from 'pocketbase';
+const pb = new Pocketbase(process.env.NEXT_PUBLIC_PB_DOMAIN);
+
 function NewsLetter() {
+  const router = useRouter();
+
   return (
     <div className="mx-auto flex justify-center">
       <input
-        className="z-[2] ml-[-50px] rounded-full border-4 border-white dark:border-2
-        bg-gradient-to-r from-[#28313B] to-[#485461] px-6 py-2 text-lg font-black text-white transition-all
-      hover:scale-110 hover:cursor-pointer focus:outline-none dark:from-[#5ea38a] dark:to-[#beb262] lg:px-10 lg:py-3"
+        className="z-[2] ml-[-50px] rounded-full border-4 border-white bg-gradient-to-r from-[#28313B]
+        to-[#485461] px-6 py-2 text-lg font-black text-white transition-all hover:scale-110 hover:cursor-pointer
+      focus:outline-none active:scale-95 dark:border-2 dark:from-[#5ea38a] dark:to-[#beb262] lg:px-10 lg:py-3"
         type="submit"
         value="عضویت"
+        onClick={async () => {
+          if (!pb.authStore.isValid || !pb.authStore.model)
+            return router.push('/sign-up');
+
+          if (pb.authStore.isValid && pb.authStore.model)
+            return router.push('/user/profile');
+        }}
       />
+
       <input
-        className="min-w-[80%] rounded-full bg-gradient-to-r from-[#5F4A55] to-[#DD8B84] pr-16 text-white
-        placeholder-white placeholder:text-xs focus:outline-none dark:from-[#a67fbe] dark:to-[#51b6af]
+        dir="ltr"
+        id="joinNewsletter"
+        className="min-w-[80%] rounded-full bg-gradient-to-r from-[#5F4A55] to-[#DD8B84] pl-5 pr-16 text-white
+        placeholder-white placeholder:text-right placeholder:text-xs focus:outline-none dark:from-[#a67fbe] dark:to-[#51b6af]
         md:placeholder:text-base lg:min-w-[50%]"
         type="email"
-        name=""
-        id="joinNewsletter"
         placeholder="با عضویت در خبرنامه ی ایمیلی، آخرین مقالات را در ایمیل خود دریافت کنید"
       />
     </div>
