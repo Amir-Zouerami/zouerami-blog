3ad1bbe925cd288e6800d262069a3bb8fd67fa36
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2023-12-08 18:07:50 +0330

    LOGIC/UI: designed and implemented portfolio section.

---
+++ app/portfolio/[slug]/page.tsx
@@ -0,0 +1,80 @@
+import { PROJECT } from '@/utility/types';
+import { createFileURL } from '@/utility/utils';
+import { Metadata } from 'next';
+import Image from 'next/image';
+import Pocketbase from 'pocketbase';
+import { parseOptions } from '@/utility/html-react-parser';
+import parse from 'html-react-parser';
+
+interface SingleProjectParam {
+  params: { slug: string };
+}
+
+const getProject = async (postSlug: string) => {
+  let post: PROJECT;
+
+  try {
+    const pb = new Pocketbase(process.env.NEXT_PUBLIC_PB_DOMAIN);
+
+    await pb.admins.authWithPassword(
+      process.env.PB_ADMIN_EM as string,
+      process.env.PB_ADMIN_PS as string
+    );
+
+    post = await pb
+      .collection('projects')
+      .getFirstListItem<PROJECT>(
+        pb.filter('slug = {:slug}', { slug: postSlug }),
+        {
+          skipTotal: true,
+        }
+      );
+
+    return post;
+  } catch (error) {
+    console.log('cached getProject func failed', error);
+    throw new Error('cached getProject func failed');
+  }
+};
+
+export async function generateMetadata({
+  params,
+}: SingleProjectParam): Promise<Metadata> {
+  let project: PROJECT = await getProject(params.slug);
+
+  return {
+    title: project.title,
+    description: project.summary,
+    // TODO: GENERATE METADATA ?
+    // openGraph: {
+    //   images: ['/some-specific-page-image.jpg', ...previousImages],
+    // },
+  };
+}
+
+async function page({ params }: SingleProjectParam) {
+  const { slug } = params;
+  let project: PROJECT = await getProject(slug);
+  const parsedHTML = parse(project.content, parseOptions);
+
+  return (
+    <div className="mx-auto mt-20 max-w-[95%] lg:max-w-[1200px]">
+      <div className="mx-auto max-h-[500px] max-w-[900px]">
+        <Image
+          src={createFileURL(project.id, project.collectionId, project.cover)}
+          width={0}
+          height={0}
+          sizes="100%"
+          alt={project.title}
+          className="h-full w-full rounded-3xl"
+        />
+      </div>
+
+      <div className="mx-auto max-w-[95%] text-justify leading-[3] 2xl:text-lg 2xl:leading-10">
+        {parsedHTML}
+      </div>
+    </div>
+  );
+}
+
+export default page;
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,16 +1,39 @@
+import Project from '@/components/Project Showcase/Project';
+import { PROJECT } from '@/utility/types';
 import { Metadata } from 'next';
+import Pocketbase from 'pocketbase';
 
 export const metadata: Metadata = {
   title: 'پورتفولیو',
   description: 'Portfolio of Amir Zouerami',
 };
 
-function page() {
+async function page() {
+  let projects;
+  try {
+    const pb = new Pocketbase(process.env.NEXT_PUBLIC_PB_DOMAIN);
+    projects = await pb
+      .collection('projects')
+      .getFullList<PROJECT>({ filter: 'featured = true' });
+  } catch (error) {}
+
   return (
-    <section className="container mx-auto max-w-[1200px]">
-      <div className="flex h-96 items-center  justify-center">
-        <p className="font-bold">این بخش در حال راه اندازی است ... </p>
+    <section className="container mx-auto max-w-[1000px]">
+      <div className="my-10 flex flex-col items-center justify-center gap-5 px-5 leading-9">
+        <h1 className="text-3xl font-black">پورتفولیو</h1>
+        <p>
+          در این بخش می تونید پروژه های اصلی من رو مشاهده کنید. تمام پروژه های
+          لیست شده در این بخش اوپن سورس هستن و به گیت هاب لینک دارن.
+        </p>
       </div>
+
+      {projects === undefined ? (
+        <div className="my-20 text-center text-lg font-bold">
+          <p>متاسفانه خطایی در دریافت داده ها رخ داده است.</p>
+        </div>
+      ) : (
+        projects.map(project => <Project key={project.id} project={project} />)
+      )}
     </section>
   );
 }
