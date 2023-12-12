baf71f2305a80499767d8940204800c0b88e4a3d
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2023-12-12 16:14:07 +0330

    UI: UI improvements for blog titles (`BlogHeading`).

--- components/Blog/BlogHeading.tsx
+++ components/Blog/BlogHeading.tsx
@@ -15,27 +15,29 @@ function BlogHeading({
   as: string;
 }) {
   return (
-    <a className="flex" href={href} id={`${id ?? null}`}>
-      <Image
-        // This abomination of a code is because of next.js. @see: https://stackoverflow.com/a/73618982/13218429
-        width={0}
-        height={0}
-        sizes="100vw"
-        style={{ width: '20px', height: 'auto' }}
-        src={link}
-        alt=""
-        className="ml-2 inline-block"
-      />
-      {as === 'h2' ? (
-        <h2 className="my-10 inline-block text-2xl font-black md:text-3xl">
-          {children}
-        </h2>
-      ) : (
-        <h3 className="my-10 inline-block text-2xl font-black md:text-3xl">
-          {children}
-        </h3>
-      )}
-    </a>
+    <div className="mb-5 mt-14 flex">
+      <a href={href} id={`${id ?? null}`} className="flex">
+        <Image
+          // This abomination of a code is because of next.js. @see: https://stackoverflow.com/a/73618982/13218429
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: '20px', height: 'auto' }}
+          src={link}
+          alt=""
+          className="ml-2 inline-block"
+        />
+        {as === 'h2' ? (
+          <h2 className="inline-block text-right text-2xl font-black leading-10 md:text-3xl">
+            {children}
+          </h2>
+        ) : (
+          <h3 className="inline-block text-right text-2xl font-black leading-10 md:text-3xl">
+            {children}
+          </h3>
+        )}
+      </a>
+    </div>
   );
 }
 
